4c0191e6114afa01336cf3b90f74d8839662ee9c
kevin <xuanji00@gmail.com>
2017-07-07 16:33:02 +0800

    增加输入文本

--- work/gensrt.py
+++ work/gensrt.py
@@ -2,16 +2,17 @@
 # -*- coding: utf-8 -*-
 
 '''
-生成srt字幕模板的脚本。
+生成srt字幕文件的脚本。
+默认设置是：一行文本对应一行字幕，字幕时间间隔一致。
 根据需要的开始时间和字幕时长、字幕条数，生成srt文件的内容如下：
 
 1
 09:20:00,000 --> 09:22:00,000
-
+字幕显示了
 
 2
 09:22:00,000 --> 09:24:00,000
-
+是什么内容呢？
 
 '''
 
@@ -39,32 +40,48 @@ def str2timestamp(strtime):
 	return time.mktime(time_array)
 
 
-def caculate_srt(fname, time_begin, duration, number):
+def caculate_srt(foutname, time_begin, duration, number, finname=None):
 	dtime = duration/number
+	lines = []
 
-	with open(fname, "w") as f:
+	if finname:
+		with open(finname, 'r') as f:
+			lines = f.readlines()
+			                    
+	with open(foutname, "w") as f:
 		for i in range(number):
 			f.write("%d\n" % (i+1))
 			tm_begin = time_begin + dtime*i
 			tm_end   = tm_begin + dtime
 			f.write("%s --> %s\n" % (timestamp2str_time(tm_begin), timestamp2str_time(tm_end)))
-			f.write("\n\n")
+			if len(lines)>i:
+				f.write(lines[i])
+			else:
+				f.write("\n")
+
+			f.write("\n")
 
 
 def test_make_srt():
 	strbegin = timestamp2str_date(time.time()) + " " + "23:23:00"
 	print(strbegin)
 	begin_time = str2timestamp(strbegin)
-	caculate_srt("./my.srt", begin_time, 120.6, 11)	
+	caculate_srt("./my.srt", begin_time, 120.6, 10)	
+	# caculate_srt("./my.srt", begin_time, 120.6, 11, "./gensrt_in.txt")	
 
 
 if __name__ == '__main__':
 	if len(sys.argv) < 5:
-		print("Usage: %s begin_time(xx:xx:xx) duration(s) number out_filename"  % sys.argv[0])
+		print("Usage: %s begin_time(xx:xx:xx) duration(s) number out_srtname [in_txtname]"  % sys.argv[0])
 		sys.exit(5)
 
 	strbegin = timestamp2str_date(time.time()) + " " + sys.argv[1]
 	begin_time = str2timestamp(strbegin)
-	caculate_srt(sys.argv[4], begin_time, float(sys.argv[2]), int(sys.argv[3]))	
+
+	inputname = None
+	if len(sys.argv) == 6:
+		inputname = sys.argv[5]
+
+	caculate_srt(sys.argv[4], begin_time, float(sys.argv[2]), int(sys.argv[3]), inputname)
 
 	# test_make_srt()

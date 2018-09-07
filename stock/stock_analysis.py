fac73a28710711e0c9d6ec17554fefbb7204c194
dengjoe <deng_joe@163.com>
2018-09-07 17:32:40 +0800

    add stocks caculate

--- stock/stock_analysis.py
+++ stock/stock_analysis.py
@@ -16,33 +16,94 @@ mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定默认字体
 mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
 
 
+# stock原始数据，保存为DataFrame格式
+class StockOriginData(object):
+    def __init__(self, sid):
+        self._sid = sid
+        __read_stock_data(sid)
+
+    def __read_stock_data(sid):
+        # 读取负债表数据
+        self._fzb = pd.read_csv(r'data/%s_fzb.csv' % (stock_id),
+            encoding='utf-8', header=0, index_col=None)
+        add_report_time(self._fzb, "报表日期")
+
+        # 读取利润表数据
+        self._lrb = pd.read_csv(r'data/%s_lrb.csv' % (stock_id),
+            encoding='utf-8', header=0, index_col=None)
+        add_report_time(self._lrb, "报表期截止日")
+
+        # 读取流量表数据
+        self._llb = pd.read_csv(r'data/%s_llb.csv' % (stock_id),
+            encoding='utf-8', header=0, index_col=None)
+        add_report_time(self._llb, "报表期截止日") 
+        return
+
+    @property
+    def fzb(self):
+        return self._fzb;
+    @property
+    def lrb(self):
+        return self._lrb;
+    @property
+    def llb(self):
+        return self._llb;
 
 
-# 读取csv文件，转化为DataFrame格式
-def get_all_data(stock_id):
-    # 读取利润表数据
-    lrb = pd.read_csv(r'data/%s_lrb.csv' % (stock_id),
-        encoding='utf-8', header=0, index_col=None)
-
-    list_lrb = []
-    for i in lrb['报表期截止日']:
+def add_report_time(data, index_name):
+    list_time = []
+    for i in data[index_name]:
         str_i = str(i)
-        # i_ = i[:4] + '-' + i[4:6] + '-' + i[6:8]
-        list_lrb.append(str(i))
-    list_lrb_0 = []
-    for i in list_lrb:
-        i_ = i[:4] + '-' + i[4:6] + '-' + i[6:8]
-        list_lrb_0.append(i_)
+        list_time.append(str_i[:4] + '-' + str_i[4:6] + '-' + str_i[6:8])
+    print(list_time)
 
-    lrb['报告时间'] = [pd.to_datetime(t) for t in list_lrb_0]
+    data['报告时间'] = [pd.to_datetime(t) for t in list_time]
+    data.index = data['报告时间']
+    return
+    
 
-    lrb.index = lrb['报告时间']
+# 
+class StockCalculate(object):
+    # 按类别提取数据，month 12月为年报数据，6月为半年报，3、9月为季报。
+    def __init__(self, data, month):
+        self._fzb = data.fzb[data.fzb.index.month == month]
+        self._lrb = data.lrb[data.lrb.index.month == month]
+        self._llb = data.llb[data.llb.index.month == month]
 
-    data = lrb[::-1]
-    return data
+    def caculate(self):
+        result = pd.DataFrame()
+        result.index = self._fzb.data['报告时间']
+
+        # 负债表
+        result['货币资金'] = self._fzb.data['货币资金']/1000000
+        result['应收账款'] = self._fzb.data['应收账款']/1000000
+        result['资产总计'] = self._fzb.data['资产总计']/1000000
+        result['应付利息'] = self._fzb.data['应付利息']/1000000
+        result['应付账款'] = self._fzb.data['应付账款']/1000000
+        result['负债合计'] = self._fzb.data['负债合计']/1000000
+        
+
+        # 利润表
+        result['营业总收入'] = self._lrb.data['营业总收入']/1000000
+        result['营业收入'] = self._lrb.data['营业收入']/1000000
+        result['营业总成本'] = self._lrb.data['营业总成本']/1000000
+        result['营业成本'] = self._lrb.data['营业成本']/1000000
+
+        result['营业利润'] = self._lrb.data['营业利润']/1000000
+        result['净利润'] = self._lrb.data['净利润']/1000000
+        result['归母净利润'] = self._lrb.data['归属于母公司所有者的净利润']/1000000
+          
+        result['营业利润率'] = result['营业利润'] / result['营业收入']
+        result['净利率'] = result['归母净利润'] / result['营业收入']
+        result['毛利率'] = (result['营业收入'] - result['营业成本']) / result['营业收入']
+        
+        # 流量表
+        
+        return result
 
 
-# 根据月份筛选数据，如12月为年报数据
+
+# 根据月份筛选数据，如12月为年报数据，6月为半年报，3、9月为季报。
 def get_data_month(data, month):
     data_month = data[data.index.month == month]
     return data_month
@@ -118,13 +179,13 @@ def data_bar(sid, data, ratio, year, kind='bar'):
 # 股票代码，输入年份（如年报数据只到2016年，则输入2017）
 def plot_stock(sid, year):
     # 读取利润表数据
-    data = get_all_data(sid)
+    data = read_stock_data(sid)
     # 将年报数据筛选出来
     data_year = get_data_month(data, 12)
     result = get_data_ratio(data_year)
 
-    data_plot(sid, result, '营业收入', year)
-    data_bar(sid, result, '归母净利润', year)
+    # data_plot(sid, result, '营业收入', year)
+    # data_bar(sid, result, '归母净利润', year)
  
 
 

81c6e0cb777c9f176d9038772ad8f2e4f50ec4a6
dengjoe <deng_joe@163.com>
2018-11-26 11:20:17 +0800

    make clean

--- matplotlib/plt01.py
+++ matplotlib/plt01.py
@@ -9,6 +9,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from pylab import mpl 
+import matplotlib.ticker as mtick 
 
 # 设置中文显示
 mpl.rcParams['font.sans-serif'] = ['SimHei'] 
@@ -41,26 +42,26 @@ def plot01():
 
 
 def plot02():
-	input_values = [1,2,3,4,5]
+	x_0 = [1,2,3,4,5]
 	plt_set_param("Square Numbers", "value", "Square of value")
-	plt.plot(input_values, squares)
+	plt.plot(x_0, squares)
 	plt.savefig("plt01_plot02.png")
 	# plt.show()
 
 def plot03():
-	in_values = [1,2,3,4,5]
+	x_0 = [1,2,3,4,5]
 	y1 = [12,4,6,5,9]
 	plt_set_param("折线图——平方", "value", "Square of value")
-	plt.plot(in_values, squares)
-	plt.plot(in_values, y1, color='red', linewidth=1.0, linestyle='--') # 叠加第2条折线图
+	plt.plot(x_0, squares)
+	plt.plot(x_0, y1, color='red', linewidth=1.0, linestyle='--') # 叠加第2条折线图
 	plt.savefig("plt01_plot03.png")
 
 
 # 散点图
 def scatter01():
-	input_values = [1,2,3,4,5]
+	x_0 = [1,2,3,4,5]
 	plt_set_param("Square Numbers", "value", "Square of value")
-	plt.scatter(input_values, squares, c=(0,0,0.8))
+	plt.scatter(x_0, squares, c=(0,0,0.8))
 	plt.show()
 
 def scatter02():
@@ -151,6 +152,68 @@ def test_multi_2():
 	plt.gca().add_artist(l1)
 	plt.show()
 
+def test_2type():
+	# 数据
+	a=[128.3,3.38,63.8,0.07,0.16,6.74,186.18] 
+	b=[0.12,-12.44,1.82,16.67,6.67,-6.52,4.04]
+	l=[i for i in range(7)]
+
+	fmt='%.2f%%'
+	yticks = mtick.FormatStrFormatter(fmt)  #设置百分比形式的坐标轴
+	x_names=[u'粮食',u'棉花',u'油料',u'麻类',u'糖料',u'烤烟',u'蔬菜']
+
+	fig = plt.figure()  
+
+	ax1 = fig.add_subplot(111)  
+	ax1.plot(l, b,'or-',label=u'增长率');
+	ax1.yaxis.set_major_formatter(yticks)
+
+	for i,(_x,_y) in enumerate(zip(l,b)):  
+	    plt.text(_x,_y,b[i],color='black',fontsize=10,)  #将数值显示在图形上
+
+	ax1.legend(loc=1)
+	ax1.set_ylim([-20, 30]);
+	ax1.set_ylabel('增长率');
+	plt.legend(prop={'family':'SimHei','size':8})  #设置中文
+
+	ax2 = ax1.twinx() # this is the important function  
+	plt.bar(l,a,alpha=0.3,color='blue',label=u'产量')  
+	ax2.legend(loc=2)
+	ax2.set_ylim([0, 2500])  #设置y轴取值范围
+	plt.legend(prop={'family':'SimHei','size':8},loc="upper left") 
+	plt.xticks(l, x_names)
+	
+	plt.show()	
+
+
+def test_axes():
+	fig = plt.figure(figsize=(6,8))
+	x_0 = [0,1,2,3,4]
+
+	ax1 = fig.add_subplot(411)   
+	ax1.plot([1,2,3,4,5])
+
+	ax1 = fig.add_subplot(412)   
+	ax1.plot(x_0, [1,8,10,20,2])
+
+	ax1 = fig.add_subplot(413)   
+	ax1.bar(x_0, [28,14,56,86,45], 0.15, color='b')
+
+	ax1 = fig.add_subplot(414)   
+	ax1.plot(x_0, [21,14,35,10,2])	
+	plt.show()	
+
+def test_figure():
+	fig = plt.figure()
+	ax1 = fig.add_subplot(211)
+	ax2 = fig.add_axes([0.1, 0.1, 0.7, 0.3])
+
+	print("fig.add_subplot():", type(ax1))
+	print("fig.add_axes():", type(ax2))
+	print(fig.axes)
+
+	plt.show()
+
 
 if __name__ == '__main__':
 	# plot01()
@@ -162,5 +225,9 @@ if __name__ == '__main__':
 	# bar01()	
 	# bar02()
 	# test_legend()
-	test_multi_plot()
-	test_multi_2()
+	# test_multi_plot()
+	# test_multi_2()
+	# 
+	# test_2type()
+	# test_axes()
+	test_figure()
